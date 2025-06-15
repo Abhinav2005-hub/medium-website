a0d202f4060083aa99949c102c28423be2e6c0d0
Abhinav2005-hub <abhinavsaini995@gmail.com>
2025-06-15 18:42:44 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-const app =express();
+const app = express();
 const port = 3000;
 
 app.use(express.json());
@@ -21,7 +21,11 @@ app.put('/api/v1/user/blog', (req, res) => {
     res.send('Hello Express');
 })
 
-app.post('/api/v1/user/blog/blog', (req, res) => {
+app.get('/api/v1/user/blog', (req, res) => {
+    res.send('Hello Express');
+})
+
+app.get('/api/v1/user/blog/blog', (req, res) => {
     res.send('Hello Express');
 })
 
