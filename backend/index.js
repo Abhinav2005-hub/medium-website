08e27e808e6ee1798b629d0d7e119c7e85ac23ad
Abhinav2005-hub <abhinavsaini995@gmail.com>
2025-06-23 14:48:57 +0530

    node modules deleted

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,46 @@
 import express from 'express';
-import { PrismaClient } from `@prisma/client/edge`
+import { PrismaClient } from '@prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
 const app = express();
+
 const port = 3000;
+
+const prisma = new PrismaClient().$extends(withAccelerate());
+
 
 app.use(express.json());
 
-app.post('/api/v1/user/signup', (req, res) => {
-    res.send('Hello Express');
-})
+const databaseUrl = process.env.DATABASE_URL;
+console.log("Database URL: ", databaseUrl);
+
+app.post('/api/v1/user/signup', async (req, res) => {
+    // const { email, password } = req.body;
+    const email = req.body.email;
+    const password = req.body.password;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+        const user = await prisma.user.create({
+            data: {
+                email,
+                password: hashedPassword,
+            },
+        });
+
+        const token = jwt.sign({ id: user.id }, 'secret');
+
+        res.json({ jwt: token });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error creating user');
+    }
+});
+
 
 app.post('/api/v1/user/signin', (req, res) => {
-    const prisma = new PrismaClient({
-        datasourceUrl: env.DATABASE_URL,
-    }).$extends(withAccelerate())
     res.send('Hello Express');
 })
 
