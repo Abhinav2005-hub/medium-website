a3ddcdd71bbd90a051bb81410f33da8da9ee56ed
Abhinav2005-hub <abhinavsaini995@gmail.com>
2025-07-23 14:12:12 +0530

    add appbar

--- frontend/src/pages/Blogs.jsx
+++ frontend/src/pages/Blogs.jsx
@@ -1,11 +1,30 @@
-import { BlogCard } from "./components/BlogCard.jsx"
+import { BlogCard } from "./components/BlogCard.jsx";
+import { Appbar } from "./components/Appbar";
+
 export const Blogs = () => {
     return <div>
-        <BlogCard
-             authorName={"abhinav"}
-             title={"title of the blog"}
-             content={"content of the blog"}
-             publishedDate={"22nd July 2025"}
-        />
+        <Appbar />
+        <div className="flex justify-center">
+        <div className="max-w-xl">
+            <BlogCard
+                 authorName={"Abhinav Saini"}
+                 title={"How an ugly single page website maeks $5000 a month without affiliate marketing"}
+                 content={"How an ugly single page website maeks $5000 a month without affiliate marketing How an ugly single page website maeks $5000 a month without affiliate marketing"}
+                 publishedDate={"22nd July 2025"}
+            />
+             <BlogCard
+                 authorName={"Abhinav Saini"}
+                 title={"How an ugly single page website maeks $5000 a month without affiliate marketing"}
+                 content={"How an ugly single page website maeks $5000 a month without affiliate marketing How an ugly single page website maeks $5000 a month without affiliate marketing"}
+                 publishedDate={"22nd July 2025"}
+            />
+             <BlogCard
+                 authorName={"Abhinav Saini"}
+                 title={"How an ugly single page website maeks $5000 a month without affiliate marketing"}
+                 content={"How an ugly single page website maeks $5000 a month without affiliate marketing How an ugly single page website maeks $5000 a month without affiliate marketing"}
+                 publishedDate={"22nd July 2025"}
+            />
+        </div>
+        </div>
     </div>
-}
+}   
---
+++ frontend/src/pages/components/Appbar.jsx
@@ -0,0 +1,12 @@
+import { Avatar } from "./BlogCard.jsx"
+
+export const Appbar = () => {
+    return <div className="border-b flex justify-between px-10 py-4">
+        <div className="flex flex-col justify-center">
+            Medium
+        </div>
+        <div>
+            <Avatar size={"big"} name = "abhinav" />
+        </div>
+    </div>
+}
